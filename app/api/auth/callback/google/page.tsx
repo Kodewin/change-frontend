4e46bd8e8ea8df8cfc24dadc9efc86a176814ce4
Jerit Baiju <jeritalumkal@gmail.com>
2024-07-17 10:51:59 +0530

    Fix: Use environment variable for API host
    Update the code to use the `NEXT_PUBLIC_API_HOST` environment variable instead of a hardcoded URL for the login endpoint. This ensures the application can connect to the correct API server regardless of the deployment environment.

--- app/api/auth/callback/google/page.tsx
+++ app/api/auth/callback/google/page.tsx
@@ -9,7 +9,7 @@ const Page = () => {
   const searchParams = useSearchParams();
   useEffect(() => {
     const fetch_tokens = async (auth_code: any) => {
-      const response = await fetch('http://127.0.0.1:8000/api/auth/google/login', {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_HOST}/api/auth/google/login`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
