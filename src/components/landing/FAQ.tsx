eb9922f4087e64178efc9c7841622a667f5000c1
Jerit Baiju <jeritalumkal@gmail.com>
2024-07-23 22:52:36 +0530

    add: Radix UI components
    Refactor frontend to incorporate Radix UI components for improved accessibility and performance. Update dependencies and remove legacy code related to Flowbite.

--- src/components/landing/FAQ.tsx
+++ src/components/landing/FAQ.tsx
@@ -1,126 +1,42 @@
+import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '../ui/accordion';
 
 const FAQ = () => {
   return (
-    <div id='accordion-collapse' data-accordion='collapse'>
-      <div>
-        <h1 className='text-3xl pb-10'>Frequently Asked Questions</h1>
-      </div>
-      <h2 id='accordion-collapse-heading-1'>
-        <button
-          type='button'
-          className='flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 rounded-t-xl focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3'
-          data-accordion-target='#accordion-collapse-body-1'
-          aria-expanded='true'
-          aria-controls='accordion-collapse-body-1'>
-          <span>Lorem ipsum dolor sit amet, consectetur adipiscing elit</span>
-          <svg
-            data-accordion-icon
-            className='w-3 h-3 rotate-180 shrink-0'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 10 6'>
-            <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 5 5 1 1 5' />
-          </svg>
-        </button>
+    <>
+      <h2 className='text-3xl'>
+        Frequently Asked Questions <i className='fa-solid fa-question'></i>
       </h2>
-      <div id='accordion-collapse-body-1' className='hidden' aria-labelledby='accordion-collapse-heading-1'>
-        <div className='p-5 border border-b-0 border-gray-200 dark:border-gray-700 dark:bg-gray-900'>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>
-            Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
-            eget dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper. Quisque vehicula massa non
-            ullamcorper elementum. Sed urna magna, convallis ac justo at, dictum semper erat.
-          </p>
-          <p className='text-gray-500 dark:text-gray-400'>
-            Check out this guide to learn how to
-            <a href='/docs/getting-started/introduction/' className='text-blue-600 dark:text-blue-500 hover:underline'>
-              get started
-            </a>{' '}
-            and start developing websites even faster with components on top of Tailwind CSS.
-          </p>
-        </div>
+      <div className='px-10 py-5'>
+        <Accordion type='single' collapsible className='w-full'>
+          <AccordionItem value='item-1'>
+            <AccordionTrigger>Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl</AccordionTrigger>
+            <AccordionContent>
+              Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
+              eget dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper.Quisque vehicula massa non
+              ullamcorper elementum.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-2'>
+            <AccordionTrigger>Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl</AccordionTrigger>
+            <AccordionContent>
+              Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
+              eget dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper.Quisque vehicula massa non
+              ullamcorper elementum.
+            </AccordionContent>
+          </AccordionItem>
+          <AccordionItem value='item-3'>
+            <AccordionTrigger>Etiam vehicula consectetur urna at vehicula?</AccordionTrigger>
+            <AccordionContent>
+              Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
+              eget dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper.Quisque vehicula massa non Etiam
+              vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam eget
+              dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper.Quisque vehicula massa non
+              ullamcorper elementum.
+            </AccordionContent>
+          </AccordionItem>
+        </Accordion>
       </div>
-      <h2 id='accordion-collapse-heading-2'>
-        <button
-          type='button'
-          className='flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-b-0 border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3'
-          data-accordion-target='#accordion-collapse-body-2'
-          aria-expanded='false'
-          aria-controls='accordion-collapse-body-2'>
-          <span>Is there a Figma file available?</span>
-          <svg
-            data-accordion-icon
-            className='w-3 h-3 rotate-180 shrink-0'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 10 6'>
-            <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 5 5 1 1 5' />
-          </svg>
-        </button>
-      </h2>
-      <div id='accordion-collapse-body-2' className='hidden' aria-labelledby='accordion-collapse-heading-2'>
-        <div className='p-5 border border-b-0 border-gray-200 dark:border-gray-700'>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>
-            Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
-            eget dui et nunc rhoncus interdum a a urna.
-          </p>
-          <p className='text-gray-500 dark:text-gray-400'>
-            Check out the{' '}
-            <a href='https://flowbite.com/figma/' className='text-blue-600 dark:text-blue-500 hover:underline'>
-              Figma design system
-            </a>{' '}
-            based on the utility classes from Tailwind CSS and components from Flowbite.
-          </p>
-        </div>
-      </div>
-      <h2 id='accordion-collapse-heading-3'>
-        <button
-          type='button'
-          className='flex items-center justify-between w-full p-5 font-medium rtl:text-right text-gray-500 border border-gray-200 focus:ring-4 focus:ring-gray-200 dark:focus:ring-gray-800 dark:border-gray-700 dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 gap-3'
-          data-accordion-target='#accordion-collapse-body-3'
-          aria-expanded='false'
-          aria-controls='accordion-collapse-body-3'>
-          <span>Etiam vehicula consectetur urna at vehicula.?</span>
-          <svg
-            data-accordion-icon
-            className='w-3 h-3 rotate-180 shrink-0'
-            aria-hidden='true'
-            xmlns='http://www.w3.org/2000/svg'
-            fill='none'
-            viewBox='0 0 10 6'>
-            <path stroke='currentColor' stroke-linecap='round' stroke-linejoin='round' stroke-width='2' d='M9 5 5 1 1 5' />
-          </svg>
-        </button>
-      </h2>
-      <div id='accordion-collapse-body-3' className='hidden' aria-labelledby='accordion-collapse-heading-3'>
-        <div className='p-5 border border-t-0 border-gray-200 dark:border-gray-700'>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>
-            Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
-            eget dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper. Quisque vehicula massa non
-            ullamcorper elementum. Sed urna magna, convallis ac justo at, dictum semper erat.
-          </p>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>
-            Etiam vehicula consectetur urna at vehicula. Curabitur accumsan eleifend nisl, in vestibulum tortor tristique quis. Nam
-            eget dui et nunc rhoncus interdum a a urna. Mauris consequat sapien at porta ullamcorper. Quisque vehicula massa non
-            ullamcorper elementum. Sed urna magna, convallis ac justo at, dictum semper erat.
-          </p>
-          <p className='mb-2 text-gray-500 dark:text-gray-400'>Learn more about these technologies:</p>
-          <ul className='ps-5 text-gray-500 list-disc dark:text-gray-400'>
-            <li>
-              <a href='https://flowbite.com/pro/' className='text-blue-600 dark:text-blue-500 hover:underline'>
-                lorem ipsum
-              </a>
-            </li>
-            <li>
-              <a href='https://tailwindui.com/' rel='nofollow' className='text-blue-600 dark:text-blue-500 hover:underline'>
-              lorem ipsum
-              </a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
+    </>
   );
 };
 
--- src/components/landing/NavBar.tsx
+++ src/components/landing/NavBar.tsx
@@ -23,7 +23,7 @@ const NavBar = () => {
             <li>
               <a
                 href='#'
-                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500'
+                className='block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-teal-700 md:p-0 dark:text-teal md:dark:text-teal-500'
                 aria-current='page'>
                 Home
               </a>
@@ -31,7 +31,7 @@ const NavBar = () => {
             <li>
               <a
                 href='#'
-                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>
+                className='block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-teal-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent'>
                 About
               </a>
             </li>
