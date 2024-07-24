6c8f4bc6caf3178006217c940fc11a6e0afa70c5
Jerit Baiju <jeritalumkal@gmail.com>
2024-07-24 07:50:25 +0530

    Fix: Sidebar toggle animation issue
    The `!delay-[0]` syntax was causing issues with the sidebar toggle animation. This commit corrects the syntax to `!delay-&lsqb;0&rsqb;` for proper animation behavior.

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,7 +19,7 @@ const Header = (props: { sidebarOpen: string | boolean | undefined; setSidebarOp
             <span className='relative block h-5.5 w-5.5 cursor-pointer'>
               <span className='du-block absolute right-0 h-full w-full'>
                 <span
-                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-dark delay-[0] duration-200 ease-in-out dark:bg-white ${
+                  className={`relative left-0 top-0 my-1 block h-0.5 w-0 rounded-sm bg-dark delay-&lsqb;0&rsqb; duration-200 ease-in-out dark:bg-white ${
                     !props.sidebarOpen && '!w-full delay-300'
                   }`}></span>
                 <span
@@ -34,7 +34,7 @@ const Header = (props: { sidebarOpen: string | boolean | undefined; setSidebarOp
               <span className='absolute right-0 h-full w-full rotate-45'>
                 <span
                   className={`absolute left-2.5 top-0 block h-full w-0.5 rounded-sm bg-dark delay-300 duration-200 ease-in-out dark:bg-white ${
-                    !props.sidebarOpen && '!h-0 !delay-[0]'
+                    !props.sidebarOpen && '!h-0 !delay-&lsqb;0&rsqb;'
                   }`}></span>
                 <span
                   className={`delay-400 absolute left-0 top-2.5 block h-0.5 w-full rounded-sm bg-dark duration-200 ease-in-out dark:bg-white ${
